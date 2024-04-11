b412e598aff0d4d2f582cc0cf90e5dccc8f41a7a
joaotfrodrigues <joaorodrigues.developer@gmail.com>
2024-04-11 23:27:33 +0100

    Create login page

--- app/Views/layouts/layout_auth.php
+++ app/Views/layouts/layout_auth.php
@@ -5,16 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CigBurger Backoffice</title>
 
+    <!-- favicon -->
+    <link rel="shortcut icon" href="<?= base_url('assets/images/logo.png') ?>" type="image/png">
+
     <!-- bootstrap -->
     <link rel="stylesheet" href="<?= base_url('assets/libs/bootstrap/bootstrap.min.css') ?>">
 
     <!-- fontawesome -->
     <link rel="stylesheet" href="<?= base_url('assets/libs/fontawesome/all.min.css') ?>">
 
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500;700&display=swap" rel="stylesheet">
+
+
     <!-- css -->
     <link rel="stylesheet" href="<?= base_url('assets/css/login.css') ?>">
 </head>
-<body>
+<body class="login-page-background">
 
     <!-- render section -->
     <?= $this->renderSection('content') ?>
