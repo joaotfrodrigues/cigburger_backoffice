7baeedd6ed7467bb045df6e3a31691c3a8e652de
joaotfrodrigues <joaorodrigues.developer@gmail.com>
2024-04-11 23:17:34 +0100

    Create auth layout file, and restructure assets folder

---
+++ app/Views/layouts/layout_auth.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CigBurger Backoffice</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('assets/libs/bootstrap/bootstrap.min.css') ?>">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/libs/fontawesome/all.min.css') ?>">
+
+    <!-- css -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/login.css') ?>">
+</head>
+<body>
+
+    <!-- render section -->
+    <?= $this->renderSection('content') ?>
+
+    <!-- bootstrap -->
+    <script src="<?= base_url('assets/libs/bootstrap/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
