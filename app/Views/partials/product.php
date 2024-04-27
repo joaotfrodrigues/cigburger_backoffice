5b49dd87e4673e6faf87dc8a5990c16a8d12a694
joaotfrodrigues <joaorodrigues.developer@gmail.com>
2024-04-27 23:28:00 +0100

    Add product messages at the dashboard

--- app/Views/partials/product.php
+++ app/Views/partials/product.php
@@ -3,10 +3,10 @@
         <div class="d-flex">
             <div>
                 <?php
-                    $image = base_url('assets/images/products/' . $product->image);
-                    if (!file_exists(ROOTPATH . 'public/assets/images/products/' . $product->image)) {
-                        $image = base_url('assets/images/products/no_image.png');
-                    }
+                $image = base_url('assets/images/products/' . $product->image);
+                if (!file_exists(ROOTPATH . 'public/assets/images/products/' . $product->image)) {
+                    $image = base_url('assets/images/products/no_image.png');
+                }
                 ?>
                 <img src="<?= $image ?>" class="img-fluid" alt="<?= $product->image ?>">
             </div>
@@ -18,8 +18,30 @@
                     <h3 class="m-0 text-primary"><strong><?= normalize_price($product->price) ?>€</strong></h3>
                 <?php else : ?>
                     <h3 class="m-0"><?= normalize_price($product->price) ?>€/<span class="text-primary"><strong><?= normalize_price(calculate_promotion($product->price, $product->promotion)) ?>€</strong></span></h3>
-                    <span class="badge bg-success">(Com promoção de <?= intval($product->promotion) ?>%)</span>
                 <?php endif; ?>
+
+                <div class="my-2">
+                    <!-- promotion -->
+                    <?php if ($product->promotion > 0) : ?>
+                        <span class="badge bg-success">(Com promoção de <?= intval($product->promotion) ?>%)</span>
+                    <?php endif; ?>
+
+                    <!-- stock -->
+                    <span class="badge bg-dark">
+                        <?= $product->stock ?>
+                        <?= $product->stock === '1' ? 'unidade' : 'unidades' ?>
+                    </span>
+
+                    <?php if ($product->stock <= $product->stock_min_limit) : ?>
+                        <span class="badge bg-danger">Stock reduzido</span>
+                    <?php endif; ?>
+
+                    <!-- availability -->
+                    <?php if (!$product->availability): ?>
+                        <span class="badge bg-warning text-dark">Produto indisponível</span>
+                    <?php endif; ?>
+                </div>
+
                 <div class="text-end align-items-bottom">
                     <a href="<?= site_url('/products/edit/' . Encrypt($product->id)) ?>" class="btn btn-sm btn-outline-secondary px-3 m-1"><i class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
                     <a href="<?= site_url('/stocks/product/' . Encrypt($product->id)) ?>" class="btn btn-sm btn-outline-secondary px-3 m-1"><i class="fa-solid fa-cubes-stacked me-2"></i>Stock</a>
