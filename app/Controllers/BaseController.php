0058708d4f75505c962076e989fc33fc34c4000c
João Rodrigues <95132933+joaotfrodrigues@users.noreply.github.com>
2024-07-04 22:35:06 +0100

    Update BaseController.php

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = ['form', 'encryption', 'functions'];
+    protected $helpers = ['form', 'Encryption', 'Functions'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
