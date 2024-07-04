869fa46ec567766d61a1054bae5614cbb7cfc79e
João Rodrigues <95132933+joaotfrodrigues@users.noreply.github.com>
2024-07-04 23:48:48 +0100

    Update RestaurantsSeeder.php

--- app/Database/Seeds/RestaurantsSeeder.php
+++ app/Database/Seeds/RestaurantsSeeder.php
@@ -22,6 +22,9 @@ class RestaurantsSeeder extends Seeder
                 'address' => 'Rua do Restaurante 1',
                 'phone' => '990000100',
                 'email' => 'restaurante_1@gmail.com',
+                'project_id' => '100',
+                'api_key' => '$2y$10$maFA042zRVi63yWGsM/GEOiKeL4HcRv1jAkb18rT/Dwy8GTfup.0S',
+                'api_key_openssl' => '7aeabc6e7e0fae8cfea502e04312539f60aa3cdef7947ebff824b3f6aa8f03bbf665a158d20f79bf5319e03b48ba373f0908eccbfef02650b83acedf04bed1f0e9bcb2feada523b0cbc8ff9dacaf5dd5b50e905a180272157f1eff0815131cd8bbc9dfd7e2ce15124f741f6b5009cc94',
                 'created_at' => date('Y-m-d H:i:s')
             ],
             [
@@ -29,6 +32,9 @@ class RestaurantsSeeder extends Seeder
                 'address' => 'Rua do Restaurante 2',
                 'phone' => '990000200',
                 'email' => 'restaurante_2@gmail.com',
+                'project_id' => '200',
+                'api_key' => '$2y$10$maFA042zRVi63yWGsM/GEOiKeL4HcRv1jAkb18rT/Dwy8GTfup.0S',
+                'api_key_openssl' => '7aeabc6e7e0fae8cfea502e04312539f60aa3cdef7947ebff824b3f6aa8f03bbf665a158d20f79bf5319e03b48ba373f0908eccbfef02650b83acedf04bed1f0e9bcb2feada523b0cbc8ff9dacaf5dd5b50e905a180272157f1eff0815131cd8bbc9dfd7e2ce15124f741f6b5009cc94',
                 'created_at' => date('Y-m-d H:i:s')
             ]
         ];
